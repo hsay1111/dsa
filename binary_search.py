fd35fde32cf6cba041d1ab20128277372211144d
hsay1111 <hsay1111@gmail.com>
2022-03-02 09:11:16 +0530

    binsearch changes

--- binary_search.py
+++ binary_search.py
@@ -1,17 +1,30 @@
+def test_location(cards, query, mid):
+    mid_number = cards[mid]
+    print("mid:", mid, ", mid_number:", mid_number)
+    if mid_number == query:
+        if mid-1 >= 0 and cards[mid-1] == query:
+            return 'left'
+        else:
+            return 'found'
+    elif mid_number < query:
+        return 'left'
+    else:
+        return 'right'
+
 def locate_card(cards, query):
     lo, hi = 0, len(cards) -1
 
     while lo <= hi:
         mid = (lo + hi) // 2
-        mid_number = cards[mid]
+        result = test_location(cards, query, mid)
 
-        print("lo:", lo, ", hi:", hi, ", mid:", mid, ", mid_number:", mid_number)
+        print("lo:", lo, ", hi:", hi)
 
-        if mid_number == query:
+        if result == 'found':
             return mid
-        elif mid_number < query:
-            hi = mid -1
-        elif mid_number > query:
+        elif result == 'left':
+            hi = mid - 1
+        elif result == 'right':
             lo = mid + 1
 
     return -1
