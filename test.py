c23386a8a0cc59e87d893fc28ef5378e364bf9f2
hsay1111 <hsay1111@gmail.com>
2022-03-02 08:13:55 +0530

    test modify

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-import math
+
+cards = [13, 11, 10, 7, 4, 3, 1, 0]
+
+query = 7
+
+def locate_card(cards, query):
+    position = 0
+
+    while True:
+
+        if cards[position] == query:
+
+            return position
+
+        position += 1
+        
+    return -1
+
+
+print(locate_card(cards, query))
+      
+
+
